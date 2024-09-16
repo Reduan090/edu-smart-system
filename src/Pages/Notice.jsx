c424a47638327b60a276b0e4861981ae34e13567
Reduan090 <reduansazid@gmail.com>
2024-09-17 01:55:27 +0600

    Notice board added

--- src/Pages/Notice.jsx
+++ src/Pages/Notice.jsx
@@ -6,19 +6,27 @@ import img from '../assets/nn.png'
 const Notice = () => {
     return (
         <>
-            
-                <div className='flex'>
 
-                    <div className='grid grid-cols-1'>
-                        <SideBar></SideBar>
+            <div className='flex'>
+
+                <div className='grid grid-cols-1'>
+                    <SideBar></SideBar>
+                </div>
+
+                <div className='w-auto h-fit md:w-11/12 md:h-screen justify-center flex'>
+                    <img src={img} alt="" />
+                    <div>
+                        <h1></h1>
                     </div>
-
-                    <div className='w-auto h-fit md:w-11/12 md:h-screen justify-center flex'>
-                        <img src={img} alt="" />
+                    
+                    <div className='absolute top-0 left-0 w-full text-center '>
+                        <h1 className='text-4xl font-bold text-cyan-950 py-2'>
+                            NoticeBoard
+                        </h1>
                     </div>
 
                 </div>
-          
+            </div>
         </>
     );
 };
